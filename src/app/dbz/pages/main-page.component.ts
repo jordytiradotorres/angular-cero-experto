f7686b2e9f45284da731615907edca319d3d3c26
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2024-10-11 14:50:14 -0500

    feat: agregando ngClass y el index de ngFor

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -7,12 +7,18 @@ import { Character } from '../interfaces/character.interface';
 })
 
 export class MainPageComponent {
-    public characters: Character[] = [{
-        name: "Krillin",
-        power: 1000,
-    },
-    {
-        name: "Goku",
-        power: 9500
-    }]
+    public characters: Character[] = [
+        {
+            name: "Krillin",
+            power: 1000,
+        },
+        {
+            name: "Goku",
+            power: 9500
+        },
+        {
+            name: "Vegeta",
+            power: 7500
+        }
+    ]
 }
